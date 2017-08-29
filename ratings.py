6b61070b6596f166ec7cba3fb8caf622d4c9a2d0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-28 17:16:58 -0700

    added add_new_entry function and broke out print function

--- ratings.py
+++ ratings.py
@@ -5,10 +5,12 @@ import sys
 
 filename = sys.argv[1]
 
+ratings = {}
+
+
 def create_ratings_dict(filename):
     """creates dictionary of restaurants and ratings from text file."""
 
-    ratings = {}
     for line in open(filename):
         line = line.strip()
         line = line.split(":")
@@ -16,12 +18,34 @@ def create_ratings_dict(filename):
         for item in line:
             ratings[line[0]] = line[1]
 
-    #rest_list = ratings.items()
+    return ratings
+
+
+def print_sorted_ratings(ratings):
+    """ prints a sorted read out of restaurants and ratings from a dictionary"""
+
     sorted_rests = sorted(ratings)
     for rest in sorted_rests:
         print "{} is rated at {}.".format(rest, ratings[rest])
 
 
+def add_new_entry():
+    """ prompts user to add new entry to ratings dictionary """
+
+    print
+    rest_name = raw_input("Enter the restaurant do you want to rate: ")
+    print
+    rest_score = raw_input("Enter the score for this restaurant: ")
+
+    ratings[rest_name] = rest_score
+
+    print
+    print "Thanks for your submission, here are the new restaurant ratings!"
+    print
+    print
+    print_sorted_ratings(ratings)
 
 
-create_ratings_dict(filename)
+print_sorted_ratings(create_ratings_dict(filename))
+
+add_new_entry()
